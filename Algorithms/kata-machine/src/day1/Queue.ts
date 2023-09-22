404236722ef720af01b996eee6d49b85646c2771
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2023-09-22 11:34:40 -0300

    queue implemeted

--- Algorithms/kata-machine/src/day1/Queue.ts
+++ Algorithms/kata-machine/src/day1/Queue.ts
@@ -1,18 +1,44 @@
+type Node<T> = {
+    next: Node<T> | undefined;
+    item: T;
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    public tail: Node<T> | undefined;
+    public head: Node<T> | undefined;
 
     constructor() {
+        this.length = 0;
+        this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const newNode: Node<T> = { item, next: undefined }
 
-}
+        if (!this.head && !this.tail) {
+            this.head = this.tail = newNode;
+
+        } else if (this.tail) {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++;
+
+    }
+
     deque(): T | undefined {
+        const item = this.head?.item;
+        if (this.head) {
+            this.length--;
+        }
+        this.head = this.head?.next;
+        return item;
 
-}
+
+    }
     peek(): T | undefined {
-
-}
+        return this.tail?.item;
+    }
 }
