17d1840fee6e4c9c49bf38e0adaa42b5c90314f2
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2023-10-03 11:00:27 -0300

    started develop quicksort

--- Algorithms/kata-machine/src/day1/QuickSort.ts
+++ Algorithms/kata-machine/src/day1/QuickSort.ts
@@ -1,3 +1,12 @@
+
+function partition(arr: number[], lo: number, hi: number): number {
+
+}
+
+function qs(arr: number[], lo: number, hi: number): void {
+
+}
+
 export default function quick_sort(arr: number[]): void {
 
 }
