b098e1668dde70e788ffc74afc35d95baf4d3d21
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2023-10-07 09:46:15 -0300

    binarysearch tree algorithms implemented

--- Algorithms/kata-machine/src/day1/BTBFS.ts
+++ Algorithms/kata-machine/src/day1/BTBFS.ts
@@ -1,3 +1,22 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q : (BinaryNode<number> | null)[] = [head];
+
+    while (q.length) {
+        const curr = q.shift() as BinaryNode<number> | undefined | null; // pop the current node
+
+        if (!curr){
+            continue;
+        }
+
+        if (curr.value === needle) {
+            return true;
+        }
+
+        q.push(curr.left);
+        q.push(curr.right);
+    }
+
+    return false;  // not found
 
 }
+
--- Algorithms/kata-machine/src/day1/DFSOnBST.ts
+++ Algorithms/kata-machine/src/day1/DFSOnBST.ts
@@ -1,3 +1,20 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function walk(curr: BinaryNode<number> | null, needle: number): boolean {
+    if (!curr) {
+        return false
+    }
+    if (curr.value === needle) {
+        return true
+    }
 
+    if (curr.value < needle) {
+        return walk(curr.right, needle);
+    }
+
+    return walk(curr.left, needle);
+
+}
+
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return walk(head, needle);
 }
