5a12d87abc85d3ca04e666ae64cc61d7057a9f16
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2025-09-29 18:03:24 -0300

    tests(ddd/python): simple ecommmerce tests for value objctes of products created

---
+++ software arquiteture/DDD/python_example/simple_ecommerce/tests/domain/catalog/value_objects/test__product_values_objects.py
@@ -0,0 +1,36 @@
+import pytest
+from src.domain.catalog.value_objects.price import Price
+from src.domain.catalog.value_objects.product_name import ProductName
+from src.domain.catalog.value_objects.category_name import CategoryName
+
+
+# Price
+def test_price_positive():
+    price = Price(amount=100.0)
+    assert price.amount == 100.0
+    assert price.currency == "USD"
+
+
+def test_price_negative_raises():
+    with pytest.raises(ValueError):
+        Price(amount=-10)
+
+
+# Product Name
+def test_product_name_valid():
+    name = ProductName("Blue Jeans")
+    assert name.value == "Blue Jeans"
+
+
+def test_product_name_empty_raises():
+    with pytest.raises(ValueError):
+        ProductName("")
+        
+# Category Name
+def test_category_name_valid():
+    cat = CategoryName("jeans")
+    assert cat.value == "jeans"
+    
+def test_category_name_invalid():
+    with pytest.raises(ValueError):
+        CategoryName("Invalid Name")
