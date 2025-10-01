b76f6bf2caeda1712677c33550f489bfeaa6e6e3
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2025-10-01 20:41:47 -0300

    feat(ddd/python/simple_ecommerce): product repository created with a sqlmodel and a data mapper

---
+++ software arquiteture/DDD/python_example/simple_ecommerce/src/infrastructure/db/config.py
@@ -0,0 +1,24 @@
+from pydantic import (computed_field)
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_core import MultiHostUrl    
+
+class DatabaseSettings(BaseSettings):
+    DATABASE_USERNAME : str
+    DATABASE_PASSWORD: str
+    DATABASE_HOST: str
+    DATABASE_NAME: str
+    DATABASE_PORT: int
+    
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> MultiHostUrl:
+        return MultiHostUrl.build(#change later for only use a database uri
+            scheme="postgresql+psycopg2",
+            username=self.DATABASE_USERNAME,
+            password=self.DATABASE_PASSWORD,
+            host=self.DATABASE_HOST,
+            port=self.DATABASE_PORT,
+            path=self.DATABASE_NAME,
+        )
+
+db_settings = DatabaseSettings()# type: ignore[call-arg] | because we load the args from the env
---
+++ software arquiteture/DDD/python_example/simple_ecommerce/src/infrastructure/db/database.py
@@ -0,0 +1,19 @@
+from collections.abc import Generator
+from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy.engine import URL
+from src.infrastructure.db.config import db_settings
+
+url_object = URL.create(
+    "postgresql+psycopg2",
+    username=db_settings.DATABASE_USERNAME,
+    password=db_settings.DATABASE_PASSWORD, 
+    host=db_settings.DATABASE_HOST,
+    database=db_settings.DATABASE_NAME,
+)
+engine = create_engine(url_object, echo=True)
+
+def get_db() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
+#TODO: This will go in api/deps.py        
+#SessionDep = Annotated[Session, Depends(get_db)]
